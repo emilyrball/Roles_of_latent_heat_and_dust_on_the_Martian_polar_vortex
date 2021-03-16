af4b74d781b3da8e19a2fd81f13aad93efdbe2f3
Emily Ball <57496230+emilyrball@users.noreply.github.com>
2021-03-16 13:15:22 +0000

    Update analysis_functions.py

--- analysis_functions.py
+++ analysis_functions.py
@@ -203,6 +203,7 @@ def calc_streamfn(lats, pfull, vz, **kwargs):
 
     return psi
   
+
 def calc_jet_lat(u, lats, plot = False):
     '''
     Function to calculate location and strength of maximum given zonal wind
@@ -226,23 +227,21 @@ def calc_jet_lat(u, lats, plot = False):
 
     u_max = np.where(u == np.ma.max(u.values))[0][0]
 
-    u_near = u[u_max-1:u_max+2]
-    lats_near = lats[u_max-1:u_max+2]
-    # Quartic fit, with smaller lat spacing
-    coefs = np.ma.polyfit(lats_near,u_near,2)
-    fine_lats = np.linspace(lats_near[0], lats_near[-1],200)
-    quad = coefs[2]+coefs[1]*fine_lats+coefs[0]*fine_lats**2
-    # Find jet lat and max
-    jet_lat = fine_lats[np.where(quad == max(quad))[0][0]]
-    jet_max = coefs[2]+coefs[1]*jet_lat+coefs[0]*jet_lat**2
-    # Plot fit?
-    if plot:
-        print (jet_max)
-        print (jet_lat)
-        plt.plot(lats_near, u_near)
-        plt.plot(fine_lats, quad)
-        plt.show()
-
+    if u_max == 0:
+        jet_lat = 90
+        jet_max = u[-1]
+    else:
+        u_near = u[u_max-1:u_max+2]
+        lats_near = lats[u_max-1:u_max+2]
+        # Quartic fit, with smaller lat spacing
+        coefs = np.ma.polyfit(lats_near,u_near,2)
+        fine_lats = np.linspace(lats_near[0], lats_near[-1],200)
+        quad = coefs[2]+coefs[1]*fine_lats+coefs[0]*fine_lats**2
+        # Find jet lat and max
+        jet_lat = fine_lats[np.where(quad == max(quad))[0][0]]
+        jet_max = coefs[2]+coefs[1]*jet_lat+coefs[0]*jet_lat**2
+        # Plot fit?
+    
     return jet_lat, jet_max
 
 def calc_Hadley_lat(u, lats, plot = False):
@@ -282,7 +281,7 @@ def calc_Hadley_lat(u, lats, plot = False):
         if u[0] > 0:
             u_0 = 0
         else:
-            u_0 = 1
+            u_0 = -1
 
         #u_0 = np.where(u == np.ma.min(np.absolute(u)))[0][0]
 
@@ -302,19 +301,208 @@ def calc_Hadley_lat(u, lats, plot = False):
 
         minq = min(np.absolute(quad))
         jet_lat = fine_lats[np.where(np.absolute(quad) == minq)[0][0]]
-        jet_max = coefs[2]+coefs[1]*jet_lat+coefs[0]*jet_lat**2
-        # Plot fit?
-        if plot:
-            print (jet_max)
-            print (jet_lat)
-            plt.plot(lats_near, u_near)
-            plt.plot(fine_lats, quad)
-            plt.show()
+        jet_max = coefs[3]+coefs[2]*jet_lat+coefs[1]*jet_lat**2 \
+                    +coefs[0]*jet_lat**3
+
     elif u_0 == 0:
         jet_lat = 90
         jet_max = u[-1]
+
+    elif u_0 == 1:
+        u_near = u[u_0-1:u_0+2]
+        lats_near = lats[u_0-1:u_0+2]
+
+        # Quartic fit, with smaller lat spacing
+        coefs = np.ma.polyfit(lats_near,u_near,2)
+        fine_lats = np.linspace(lats_near[0], lats_near[-1],200)
+        quad = coefs[2]+coefs[1]*fine_lats+coefs[0]*fine_lats**2 
+        # Find jet lat and max
+        #jet_lat = fine_lats[np.where(quad == max(quad))[0][0]]
+
+        minq = min(np.absolute(quad))
+        jet_lat = fine_lats[np.where(np.absolute(quad) == minq)[0][0]]
+        jet_max = coefs[2]+coefs[1]*jet_lat+coefs[0]*jet_lat**2
+
     else:
+        print(u.values)
         jet_lat = np.nan
         jet_max = np.nan
 
     return jet_lat, jet_max
+  
+
+
+def moving_average(x, w):
+    return np.convolve(x, np.ones(w), 'valid') / w
+  
+def filestrings(exp, filepath, start, end, filename, **kwargs):
+    '''
+    Generates lists of strings, for Isca runs.
+    '''
+    outpath = kwargs.pop('outpath', filepath)
+
+    if start<10:
+        st='000'+str(start)
+    elif start<100:
+        st='00'+str(start)
+    elif start<1000:
+        st='0'+str(start)
+    else:
+        st=str(start)
+
+    if end<10:
+        en='000'+str(end)
+    elif end<100:
+        en='00'+str(end)
+    elif end<1000:
+        en='0'+str(end)
+    else:
+        en=str(end)
+
+
+    nfiles = end - start + 1
+    infiles = []
+    runs = []
+    out = []
+
+    for i in range(nfiles):
+        run_no = start+i
+        if run_no<10:
+            run='run000'+str(run_no)
+        elif run_no<100:
+            run='run00'+str(run_no)
+        elif run_no<1000:
+            run='run0'+str(run_no)
+        else:
+            run='run'+str(run_no)
+
+        runs.append(run)
+        out.append(outpath +'/'+exp+'/'+run+'/'+filename)
+        infiles.append(filepath+'/'+exp+'/'+run+'/'+filename)
+
+    return runs, out, infiles
+  
+def stereo_plot():
+    '''
+    Returns variables to define a circular plot in matplotlib.
+    '''
+    theta = np.linspace(0, 2*np.pi, 100)
+    center, radius = [0.5, 0.5], 0.5
+    verts = np.vstack([np.sin(theta), np.cos(theta)]).T
+    circle = mpath.Path(verts * radius + center)
+    return theta, center, radius, verts, circle
+
+def make_stereo_plot(ax, lats, lons, circle, **kwargs):
+    '''
+    Makes the polar stereographic plot and plots gridlines at choice of lats
+    and lons.
+    '''
+    linewidth = kwargs.pop('linewidth', 1)
+    linestyle = kwargs.pop('linestyle', '-')
+    color = kwargs.pop('color', 'black')
+    alpha = kwargs.pop('alpha', 1)
+
+    gl = ax.gridlines(crs = ccrs.PlateCarree(), linewidth = linewidth,
+                      linestyle = linestyle, color = color, alpha = alpha)
+
+    ax.set_boundary(circle, transform=ax.transAxes)
+
+    gl.ylocator = ticker.FixedLocator(lats)
+    gl.xlocator = ticker.FixedLocator(lons)
+
+def make_colourmap(vmin, vmax, step, **kwargs):
+    '''
+    Makes a colormap from ``vmin`` (inclusive) to ``vmax`` (exclusive) with
+    boundaries incremented by ``step``. Optionally includes choice of color and
+    to extend the colormap.
+    '''
+    col = kwargs.pop('col', 'viridis')
+    extend = kwargs.pop('extend', 'both')
+
+    boundaries = list(np.arange(vmin, vmax, step))
+
+    if extend == 'both':
+        cmap_new = cm.get_cmap(col, len(boundaries) + 1)
+        colours = list(cmap_new(np.arange(len(boundaries) + 1)))
+        cmap = colors.ListedColormap(colours[1:-1],"")
+        cmap.set_over(colours[-1])
+        cmap.set_under(colours[0])
+
+    elif extend == 'max':
+        cmap_new = cm.get_cmap(col, len(boundaries))
+        colours = list(cmap_new(np.arange(len(boundaries))))
+        cmap = colors.ListedColormap(colours[:-1],"")
+        cmap.set_over(colours[-1])
+
+    elif extend == 'min':
+        cmap_new = cm.get_cmap(col, len(boundaries))
+        colours = list(cmap_new(np.arange(len(boundaries))))
+        cmap = colors.ListedColormap(colours[1:],"")
+        cmap.set_under(colours[0])
+
+    norm = colors.BoundaryNorm(boundaries, ncolors = len(boundaries) - 1,
+                               clip = False)
+
+    return boundaries, cmap_new, colours, cmap, norm
+  
+def assign_MY(d):
+    '''
+    Calculates new MY for Isca simulations and adds this to input dataset.
+    Also returns the indices of the time axis that correspond to a new MY.
+    '''
+    t = np.zeros_like(d.time)
+    index=[]
+    for i in range(len(t)-1):
+        if d.mars_solar_long[i+1]<d.mars_solar_long[i]:
+            print(d.mars_solar_long[i].values)
+            print(d.mars_solar_long[i+1].values)
+            t[i+1] = t[i]+1
+            index.append(d.time[i])
+        else:
+            t[i+1] = t[i]
+    t1 = xr.Dataset({"MY" : (("time"), t)},
+                    coords = {"time":d.time})
+    d = d.assign(MY=t1["MY"])
+    return d, index
+
+def make_coord_MY(x, index):
+    x = x.where(x.time > index[0], drop=True)
+    x = x.where(x.time <= index[-1], drop=True)
+
+    N=int(np.max(x.MY))
+    n = range(N)
+
+    y = x.time[:len(x.time)//N]
+
+    ind = pd.MultiIndex.from_product((n,y),names=('MY','new_time'))
+    dsr = x.assign_coords({'time':ind}).unstack('time')
+    #dsr = dsr.squeeze()
+
+    return dsr, N, n
+  
+def calc_PV_max(PV, lats, plot=False):
+    '''
+    Function to calculate location and strenth of maximum given zonal-mean PV
+    PV(height) field
+    '''
+    # Restict to 10 points around maximum
+    PV_max = np.where(PV == np.ma.max(PV))[0][0]
+    PV_near = PV[PV_max-1:PV_max+2]
+    lats_near = lats[PV_max-1:PV_max+2]
+    # Quartic fit, with smaller lat spacing
+    coefs = np.ma.polyfit(lats_near,PV_near,2)
+    fine_lats = np.linspace(lats_near[0], lats_near[-1],200)
+    quad = coefs[2]+coefs[1]*fine_lats+coefs[0]*fine_lats**2
+    # Find jet lat and max
+    jet_lat = fine_lats[np.where(quad == max(quad))[0][0]]
+    jet_max = coefs[2]+coefs[1]*jet_lat+coefs[0]*jet_lat**2
+    # Plot fit?
+    if plot:
+        print (jet_max)
+        print (jet_lat)
+        plt.plot(lats_near, PV_near)
+        plt.plot(fine_lats, quad)
+        plt.show()
+
+    return jet_lat, jet_max
+
